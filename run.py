5016994beeaba4ee79e215439fd6ef60285a647e
Arunreligh <143166894+Arunreligh@users.noreply.github.com>
2023-10-09 15:58:47 +0530

    Update run.py

--- run.py
+++ run.py
@@ -213,5 +213,5 @@ def delete_single_template(template_id):
 
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port='5000')
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port='5000')
+    # app.run()
